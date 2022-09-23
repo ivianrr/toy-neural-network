fd37d83dd708995df4eabf4d49f6c521b8e21b3c
Ivan <ivianrr@gmail.com>
2022-09-23 16:37:18 +0200

    work on activation functions and forw_prop

--- toynetwork.py
+++ toynetwork.py
@@ -1,8 +1,8 @@
 from typing import Callable
 import numpy as np
 
-
 class Functions:
+    # https://medium.com/@krishnakalyan3/introduction-to-exponential-linear-unit-d3e2904b366c
     @staticmethod
     def ReLU(Z, derivative=False):
         if not derivative:
@@ -17,6 +17,19 @@ class Functions:
     @staticmethod
     def softmax(Z, derivative=False):
         return np.exp(Z) / np.sum(np.exp(Z), axis=0)
+
+    @staticmethod
+    def ELU(Z,derivative=False):
+        a=1
+        A=np.zeros_like(Z)
+        if not derivative:
+            A[Z>0]=Z[Z>0]
+            A[Z<=0]=a*(np.exp(Z[Z<=0])-1)
+        else:
+            A[Z>0]=1
+            A[Z<=0]=Functions.ELU(Z[Z<=0],False)+a
+        return A
+
 
     @staticmethod
     def cross_ent(Y, A):  # true(onehotencoded), predicted
@@ -69,10 +82,6 @@ class Network:
     def set_loss_function(self, f: Callable):
         self.loss_function = f
 
-    @staticmethod
-    def do_something():
-        print("hello")
-
     def add_layer(self, layer: Layer):
         if self.layers:
             prev = self.layers[-1].size
@@ -98,18 +107,65 @@ class Network:
                 input_value = self.layers[i-1].A
             layer.Z = layer.weights @ input_value + layer.biases
             layer.A = layer.act_f(layer.Z)
-        return layer.A
 
-    def back_propagate(self, X, Y, alpha, epochs, batchsize):
+    @staticmethod
+    def one_hot(labels: np.ndarray):
+        max=labels.max()
+        Y=np.zeros((max+1,labels.size))
+        Y[labels,np.arange(labels.size)]=1
+        return Y
+
+    def get_predictions(self):
+        A=self.layers[-1].A
+        return np.argmax(A,axis=0)
+
+    def backward_prop(self,X,Y):
+        for i, layer in reversed(enumerate(self.layers)):
+            
+            pass
+
+    def gradient_descent(self, X, Y, alpha, epochs, batch_size):
+        def batch_split(x,batch_size,axis):
+            b=batch_size
+            x=x.swapaxes(0,axis)
+            l=x.shape[0]
+            n_b=range(l//b)
+            for r in n_b:
+                yield x[r*b:(r+1)*b].swapaxes(0,axis)
+            if l%b>0:
+                yield x[l//b*b:].swapaxes(0,axis)
+
+        N=Y.size
+        Y_one_hot=self.one_hot(Y)
+        for i in range(epochs):
+            # Shuffle trainig data first
+            perms=np.random.permutation(N)
+            X=X[:,perms]
+            Y=Y_one_hot[perms]
+            # Split in batches and run every batch each epoch. 
+            for X_b,Y_b in zip(batch_split(X,batch_size,axis=1),batch_split(Y,batch_size,axis=0)):
+                Z1,A1,Z2,A2 = forward_prop(W1,b1,W2,b2,X_b)
+                dW1,db1,dW2,db2 = backward_prop(Z1, A1, Z2, A2, W1, W2, X_b, Y_b)
+                W1, b1, W2, b2 = update_params(W1,b1, W2,b2,dW1,db1,dW2,db2,alpha)
+
         pass
 
 
 if __name__ == "__main__":
-    nn = Network(input_size=5)
-    nn.add_layer(Layer(2, Functions.ReLU))
-    nn.add_layer(Layer(4, Functions.softmax))
-    nn.set_loss_function(Functions.cross_ent)
-    print(nn)
-    nn.init_params()
-    X = np.random.rand(5, 100)
-    print(nn.forward_propagate(X).shape)
+    model = Network(input_size=5)
+    model.add_layer(Layer(2, Functions.ReLU))
+    model.add_layer(Layer(2, Functions.ELU))
+    model.add_layer(Layer(4, Functions.softmax))
+    model.set_loss_function(Functions.cross_ent)
+    print(model)
+    model.init_params()
+
+    X = np.random.rand(5, 10)
+    model.forward_propagate(X)
+
+    print("Predictions")
+    print(model.get_predictions())
+
+    print("Probabilities of first prediction.")
+    print(model.layers[-1].A[:,0])
+
